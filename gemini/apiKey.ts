e2b4f7fddb99a1bcd66cf1330e00a4dd59e7fdc9
Matheus Abade <abade.mth@gmail.com>
2025-05-17 18:41:48 -0300

    quase expus minha api key

--- gemini/apiKey.ts
+++ gemini/apiKey.ts
@@ -1,1 +1,3 @@
-export const apiKey = "AIzaSyDGuiTtL1lWOAX20Cn40lQ21KSsHbA-scc";
+export const apiKey = "SUA API KEY AQUI";
+//Caso não saiba como gerar sua API Key, siga o passo a passo na documentação do projeto no GitHub
+//GitHub do projeto: https://github.com/AbadeMTH/chat-imersaoia/tree/main
