ed097c74e656a5687536b6f3ce79d52c26247826
Matheus Abade <abade.mth@gmail.com>
2025-05-20 21:08:09 -0300

    removing apikey

--- gemini/apiKey.ts
+++
@@ -1,3 +0,0 @@
-export const apiKey = "SUA API KEY AQUI";
-//Caso não saiba como gerar sua API Key, siga o passo a passo na documentação do projeto no GitHub
-//GitHub do projeto: https://github.com/AbadeMTH/chat-imersaoia 
