fb1eec15508cc75c41ac516a2f45758033e21218
Matheus Abade <abade.mth@gmail.com>
2025-05-20 21:01:44 -0300

    hiding api key

--- gemini/apiKey.ts
+++ gemini/apiKey.ts
@@ -1,3 +1,3 @@
 export const apiKey = "SUA API KEY AQUI";
 //Caso não saiba como gerar sua API Key, siga o passo a passo na documentação do projeto no GitHub
-//GitHub do projeto: https://github.com/AbadeMTH/chat-imersaoia
+//GitHub do projeto: https://github.com/AbadeMTH/chat-imersaoia 
